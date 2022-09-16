d13e776c435c4e295c197795fd7db69e47004663
Gideon Ritter <gideon.ritter@enymo.com>
2022-09-16 13:08:01 +0200

    Initial commit.

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+export default function compare(a: any, b: any) {
+    if (a < b) {
+        return -1;
+    }
+    else if (a > b) {
+        return 1;
+    }
+    else {
+        return 0;
+    }
+}
